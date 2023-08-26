d196ca78ce321d1a67c96caa5630ca4fe7512b4b
Misbah Mughal <122009223+MusbahMughal@users.noreply.github.com>
2023-08-27 00:56:32 +0500

    Add files via upload

---
+++ FIREBASE HOSTING/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDDhvXJVmllT5bGNxgDiiyx4OJ42x0ajI",
+  authDomain: "my-appliacation-e6e8f.firebaseapp.com",
+  projectId: "my-appliacation-e6e8f",
+  storageBucket: "my-appliacation-e6e8f.appspot.com",
+  messagingSenderId: "826899909961",
+  appId: "1:826899909961:web:6ae58c2539152480531e98",
+  measurementId: "G-TRCKT2B5QL"
+};
